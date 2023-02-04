2d306adb84498916d99b657fa18af187588d051c
SoonTaeYouL <smile971021@naver.com>
2023-02-04 22:36:09 +0900

    no message

---
+++ projects/myapi/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173", # 또는 "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello") #/hello라는 URL요청이 발생하면
+def hello():       # 해당함수를 실행하여 결과를 리턴하라는 의미
+    return{"message" : "안녕하세요 화이보"}
