b66fa63270df9aee646f15ad3dbb54c8936222c9
SoonTaeYouL <smile971021@naver.com>
2023-02-04 22:44:14 +0900

    no message

--- projects/myapi/frontend/src/App.svelte
+++ projects/myapi/frontend/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  let message; 
+  let message; //message라는 변수 생성
 
-  fetch("http://127.0.0.1:8000/hello").then((response)=>{
-    response.json().then((json)=>{
-      message = json.message;
+  fetch("http://127.0.0.1:8000/hello").then((response)=>{ 
+    response.json().then((json)=>{ //FastAPI의 hello API를 호출하여 돌려 받은 값을
+      message = json.message; // message변수에 넣기
     });
   });
 </script>
